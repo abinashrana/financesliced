e49fd4eda3d9f65a78eb9ce7aafee24ec5c57db9
abinashrana <abinash.avdesigns@gmail.com>
2016-07-27 14:00:05 +0545

    pushing new changes

--- markup/js/jquery.main.js
+++ markup/js/jquery.main.js
@@ -1,1 +1,34 @@
- 
+$(function() {
+
+	$("#owl-banner").owlCarousel({
+		autoPlay:3000,
+		pagination:true,
+		stopOnHover:true,
+		navigation : false, // Show next and prev buttons
+		slideSpeed : 300,
+		paginationSpeed : 400,
+		singleItem:true
+
+		// "singleItem:true" is a shortcut for:
+		// items : 1, 
+		// itemsDesktop : false,
+		// itemsDesktopSmall : false,
+		// itemsTablet: false,
+		// itemsMobile : false
+
+	});
+	
+	var owl = $("#owl-logo");
+	owl.owlCarousel({
+		itemsCustom : [
+			[0, 2],
+			[480, 3],
+			[700, 5]
+		],
+		pagination:false,
+		navigation : true
+	});
+
+	$("#myModal").modal();
+
+});
