41e4167a4cdc472ccb157d295a756272a918a220
abinashrana <abinash.avdesigns@gmail.com>
2016-08-05 13:21:11 +0545

    update day14

--- markup/js/jquery.main.js
+++ markup/js/jquery.main.js
@@ -1,13 +1,13 @@
-$(function() {
+$(function () {
 
 	$("#owl-banner").owlCarousel({
-		autoPlay:3000,
-		pagination:true,
-		stopOnHover:true,
-		navigation : false, // Show next and prev buttons
-		slideSpeed : 300,
-		paginationSpeed : 400,
-		singleItem:true
+	/*	autoPlay: 3000,*/
+		pagination: true,
+		stopOnHover: true,
+		navigation: false, // Show next and prev buttons
+		slideSpeed: 300,
+		paginationSpeed: 400,
+		singleItem: true
 
 		// "singleItem:true" is a shortcut for:
 		// items : 1, 
@@ -17,18 +17,80 @@ $(function() {
 		// itemsMobile : false
 
 	});
-	
+
 	var owl = $("#owl-logo");
 	owl.owlCarousel({
-		itemsCustom : [
-			[0, 2],
-			[480, 3],
-			[700, 5]
-		],
-		pagination:false,
-		navigation : true
+		itemsCustom: [
+				[0, 2],
+				[480, 3],
+				[680, 4],
+				[992, 5]
+			],
+		pagination: false,
+		navigation: true
+	});
+
+	$("#owl-story").owlCarousel({
+		pagination: false,
+		stopOnHover: true,
+		navigation: true, // Show next and prev buttons
+		slideSpeed: 300,
+		paginationSpeed: 400,
+		singleItem: true
+
+		// "singleItem:true" is a shortcut for:
+		// items : 1, 
+		// itemsDesktop : false,
+		// itemsDesktopSmall : false,
+		// itemsTablet: false,
+		// itemsMobile : false
+
 	});
 
 	$("#myModal").modal();
 
+	/*var $searchlink = $('#searchlink');
+
+	// hover effect
+	$searchlink.on('click', function () {
+		$(this).addClass('open');
+	}).on('mouseout', function () {
+		$(this).removeClass('open');
+	});*/
+
+	$('.parent-container').magnificPopup({
+		delegate: 'a', // child items selector, by clicking on it popup will open
+		gallery: {
+			enabled: true
+		},
+		type: 'image'
+			// other options
+	});
+
+	/*jQuery(".pull_feedback").toggle(function () {
+			jQuery("#feedback").animate({
+				left: "0px"
+			});
+			return false;
+		},
+		function () {
+			jQuery("#feedback").animate({
+				left: "-362px"
+			});
+			return false;
+		}
+	); //toggle
+	}); //document.ready
+*/
+	/*toggles*/
+	
+	/*$(".left-cnt").hide();*/
+	$(".icn-call-nav").click(function() {
+		$(".left-cnt").toggleClass('open');
+	});
+	
+	$(".icn-search").click(function() {
+		$(".searchform").toggleClass('open');
+	});
+	
 });
